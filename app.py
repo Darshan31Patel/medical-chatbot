dada689a107bc7ca8582c05314ea2720c017625c
Darshan31Patel <darshan.h3112@gmail.com>
2024-07-04 17:58:20 +0530

    faiss db code added

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ PATH = 'model/embedding.joblib'
 
 if os.path.exists(PATH):
     embeddings = joblib.load(PATH)
+    print("Model loaded")
 else: 
     embeddings = download_hugging_face_embeddings()
     save_embeddings(embeddings)
+    print("Model saved")
 
 # Load Existing vector database
 docsearch = PineconeVectorStore.from_existing_index(index_name="mchatbot",embedding=embeddings)
@@ -30,12 +32,24 @@ retriever = docsearch.as_retriever()
 
 retrieval_chain = create_retrieval_chain(retriever,document_chain)
 
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
 
 st.title("Medical ChatBot")
 input = st.text_input("Ask anything related to medicine or diseases : ")
+
 if input:
+    st.session_state['chat_history'].append(('You',input))
     print("start")
     response = retrieval_chain.invoke({"input" : input})
-    print(response['answer'])
+    # print(response['answer'])
     print("end")
+    st.session_state['chat_history'].append(('Bot',response['answer']))
     st.write(response['answer'])
+
+if st.session_state['chat_history']:
+    st.subheader("Chat history:")
+
+for role,text in st.session_state['chat_history']:
+    st.write(f'{role} : {text}')
